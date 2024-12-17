560dda76664e40b2605154f53422fae5942490a2
nozwock <57829219+nozwock@users.noreply.github.com>
2024-12-18 04:01:52 +0530

    Styling changes

--- Lua/Autorun/stacksize.lua
+++ Lua/Autorun/stacksize.lua
@@ -61,23 +61,24 @@ end
 -- Arbitrary max limit is (6 bits, i.e. 2 ^ 6 - 1) = 63
 -- Likely there due to network syncing.
 -- https://github.com/FakeFishGames/Barotrauma/blob/0e8fb6569d2810e2f8ad5fb17b4bba546cc5739a/Barotrauma/BarotraumaShared/SharedSource/Items/Inventory.cs#L13
-local stacksize = 62
+-- Picking up an even number before 63, as max
+local MAX_STACK_SIZE = 62
 
 for prefab in ItemPrefab.Prefabs do
 	if iterContainsAny(prefab.Tags, { "oxygensource", "weldingfuel" }) then
 		-- Don't change the player inventory stack size for these items
-		prefab.set_MaxStackSize(stacksize)
-		prefab.set_MaxStackSizeHoldableOrWearableInventory(stacksize) -- WearableInventory applies to Toolbelts/Backpacks
+		prefab.set_MaxStackSize(MAX_STACK_SIZE)
+		prefab.set_MaxStackSizeHoldableOrWearableInventory(MAX_STACK_SIZE) -- WearableInventory applies to Toolbelts/Backpacks
 	elseif iterContainsAny(prefab.Tags, { "mobilebattery", "handheldammo", "shotgunammo" }) then
 		-- Only double the item's stack size in player inventory, and max verywhere else
-		prefab.set_MaxStackSize(stacksize)
+		prefab.set_MaxStackSize(MAX_STACK_SIZE)
 		prefab.set_MaxStackSizeCharacterInventory(math.ceil(prefab.MaxStackSizeCharacterInventory * 2))
-		prefab.set_MaxStackSizeHoldableOrWearableInventory(stacksize)
+		prefab.set_MaxStackSizeHoldableOrWearableInventory(MAX_STACK_SIZE)
 	elseif iterContainsAny(prefab.Tags, { "smallitem" }) and prefab.MaxStackSize > 1 then
 		-- Every small item should've max stack everywhere, excluding
 		-- those that shouldn't stack at all in the first place
-		prefab.set_MaxStackSize(stacksize)
-		prefab.set_MaxStackSizeCharacterInventory(stacksize)
-		prefab.set_MaxStackSizeHoldableOrWearableInventory(stacksize)
+		prefab.set_MaxStackSize(MAX_STACK_SIZE)
+		prefab.set_MaxStackSizeCharacterInventory(MAX_STACK_SIZE)
+		prefab.set_MaxStackSizeHoldableOrWearableInventory(MAX_STACK_SIZE)
 	end
 end
