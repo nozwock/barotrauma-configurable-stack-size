4ade9f7ce53e0a8c13156feaecc3900ca9459567
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-02 19:45:40 +0530

    Add more functions to table extensions

--- Lua/Mod/ext.lua
+++ Lua/Mod/ext.lua
@@ -1,5 +1,5 @@
----@generic T
----@param tab T: table
+---@generic T: table
+---@param tab T
 ---@return T
 function table.shallowcopy(tab)
 	local new = {}
@@ -7,4 +7,34 @@ function table.shallowcopy(tab)
 		new[k] = v
 	end
 	return new
+end
+
+--- Iterator over table keys.
+---@generic K, V
+---@param t table<K, V>
+---@return fun():K
+function table.keys(t)
+	local next_fn, state, index = pairs(t)
+	return function()
+		local k, _v = next_fn(state, index)
+		index = k
+		if k ~= nil then
+			return k
+		end
+	end
+end
+
+--- Iterator over table values.
+---@generic K, V
+---@param t table<K, V>
+---@return fun():V
+function table.values(t)
+	local next_fn, state, index = pairs(t)
+	return function()
+		local k, v = next_fn(state, index)
+		index = k
+		if v ~= nil then
+			return v
+		end
+	end
 end
