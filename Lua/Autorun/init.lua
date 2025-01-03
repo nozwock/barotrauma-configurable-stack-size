9e1b17bfd4b9891056e55d7f44c30e5fb2c677c0
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-04 00:21:02 +0530

    Make mod multiplayer compatible
    By syncing config between server and client.

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,10 +1,10 @@
 require("ConfigurableStackSize.ext") -- Make extensions available for all from here on.
 local config = require("ConfigurableStackSize.config")
 local patches = require("ConfigurableStackSize.patches")
+local network = require("ConfigurableStackSize.network")
 
 local Config = config.Config
 
--- todo: handle config syncing in multiplayer
 -- todo: have separate config for sp and mp
 -- todo: look into whether the config files will be downloaded by the client too or not, and if so
 -- figure out how to exclude them
@@ -12,6 +12,23 @@ local Config = config.Config
 local cfg = Config.tryLoadFromDiskOrDefault()
 -- todo?: Should container sizes be bound by ContainerOptions.maxStackSize?
 
-patches.runBypassMaxStackSizeLimit()
-patches.runContainersPatch(cfg.data.containerOptions)
-patches.runItemPrefabsPatch(cfg)
+---@param cfg Config
+local function runClientPatches(cfg)
+	patches.runContainersPatch(cfg.data.containerOptions)
+	patches.runItemPrefabsPatch(cfg)
+end
+
+if Game.IsSingleplayer then
+	patches.runBypassMaxStackSizeLimit()
+	runClientPatches(cfg)
+elseif SERVER then
+	network.server.setSendConfigHandler(function()
+		return cfg
+	end)
+	runClientPatches(cfg)
+elseif CLIENT and Game.IsMultiplayer then
+	network.client.setReceiveConfigHandler(function(serializedConfig)
+		runClientPatches(Config.tryLoadFromString(serializedConfig))
+	end)
+	network.client.requestReceiveConfig()
+end
---
+++ Lua/ConfigurableStackSize/network.lua
@@ -0,0 +1,54 @@
+local mod = {}
+
+---@param msgId string
+local function getMessage(msgId)
+	return "ConfigurableStackSize" .. "." .. msgId
+end
+
+---@enum Message
+local Message = {
+	ServerSendConfig = getMessage("Server.SendConfig"),
+	ClientRequestConfig = getMessage("Client.RequestConfig"),
+}
+
+local server = {}
+
+-- Used Gather Resources Quickly for reference.
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=3337144393
+
+---@param cfg Config
+---@param client? Barotrauma.Networking.Client
+function server.sendConfig(cfg, client)
+	local msg = Networking.Start(Message.ServerSendConfig)
+	msg.WriteString(tostring(cfg))
+
+	Networking.Send(msg, client and client.Connection or nil) -- nil is every connected client
+end
+
+---@param getConfig fun():Config
+function server.setSendConfigHandler(getConfig)
+	Networking.Receive(Message.ClientRequestConfig, function(_msg, client)
+		server.sendConfig(getConfig(), client)
+	end)
+end
+
+local client = {}
+
+function client.requestReceiveConfig()
+	Networking.Send(Networking.Start(Message.ClientRequestConfig))
+end
+
+---@param cb fun(serializedConfig: string)
+function client.setReceiveConfigHandler(cb)
+	Networking.Receive(Message.ServerSendConfig, function(msg, _server)
+		---@cast msg Barotrauma.Networking.IReadMessage
+		cb(msg.ReadString() --[[@as string]])
+	end)
+end
+
+mod.getMessage = getMessage
+mod.Message = Message
+mod.server = server
+mod.client = client
+
+return mod
