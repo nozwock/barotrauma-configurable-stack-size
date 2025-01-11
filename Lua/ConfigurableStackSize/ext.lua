53e54d51f3291a9e9563b66cf3668cd30e19426a
nozwock <57829219+nozwock@users.noreply.github.com>
2025-01-11 20:02:16 +0530

    Add table.deepcopy

--- Lua/ConfigurableStackSize/ext.lua
+++ Lua/ConfigurableStackSize/ext.lua
@@ -9,6 +9,33 @@ function table.shallowcopy(tab)
 	return new
 end
 
+---@generic T: table
+---@param tab T
+---@param _visited? table<table, table>
+---@return T
+function table.deepcopy(tab, _visited)
+	_visited = _visited or {}
+
+	-- If the table has already been copied, return the reference to the copied table
+	if _visited[tab] then
+		return _visited[tab]
+	end
+
+	local copy = {}
+
+	-- Mark the current table as visited and store the reference to the new copy
+	_visited[tab] = copy
+
+	for k, v in pairs(tab) do
+		if type(v) == "table" then
+			copy[k] = table.deepcopy(v, _visited)
+		else
+			copy[k] = v
+		end
+	end
+
+	return copy
+end
 --- Iterator over table keys.
 ---@generic K, V
 ---@param t table<K, V>
